4d1e78a1ecb583d13e2fe3deadc4ca4b4378e560
Graham Watt <gmwatt@gmail.com>
2017-11-14 07:59:57 -0500

    Use temp files for boot/program files

--- embed_target.c
+++ embed_target.c
@@ -1,8 +1,13 @@
 
 #include <elf.h>
+#include <fcntl.h>
+#include <errno.h>	
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+
+#include <scheme.h>
 
 #define BITS (__SIZEOF_POINTER__ * __CHAR_BIT__)
 
@@ -54,7 +59,17 @@ size_t extract_elf_section(FILE *fp, uint16_t shnum, Elf_Off shoff, const char *
 	return size;
 }
 
-int main(int argc, char **argv) {
+int maketempfile(char *template, const char *contents, size_t size) {
+	int fd;
+	fd = mkstemp(template);
+	if (fd < 0) return fd;
+
+	write(fd, contents, size);
+	lseek(fd, 0, SEEK_SET);
+	return fd;
+}
+
+int main(int argc, const char **argv) {
 	FILE *fp;
 	Elf_Ehdr ehdr;
 	char *s_names;
@@ -62,23 +77,46 @@ int main(int argc, char **argv) {
 	size_t bootsize;
 	char *scheme;
 	size_t schemesize;
+	char bootfilename[] = "/tmp/chezschemebootXXXXXX";
+	int bootfd;
+	char schemefilename[] = "/tmp/schemeprogramXXXXXX";
+	int schemefd;
 
 	fp = fopen("/proc/self/exe", "rb");
 	fread(&ehdr, sizeof ehdr, 1, fp);
 	s_names = read_section_header_names(fp, ehdr);
 	bootsize = extract_elf_section(fp, ehdr.e_shnum, ehdr.e_shoff, s_names, bootname, &boot);
 	schemesize = extract_elf_section(fp, ehdr.e_shnum, ehdr.e_shoff, s_names, srcname, &scheme);
+	free(s_names);
+	fclose(fp);
 
 	if (bootsize) printf("Found boot data of size %lu\n", bootsize);
 	else printf("No boot data found\n");
 	if (schemesize) printf("Found scheme source of size %lu\n", schemesize);
 	else printf("No scheme source found\n");
 
-	if (s_names) free(s_names);
-	if (boot) free(boot);
-	if (scheme) free(scheme);
+	bootfd = maketempfile(bootfilename, boot, bootsize);
+	if (bootfd < 0) {
+		int e = errno;
+		fputs(strerror(e), stderr);
+		return -e;
+	}
+	free(boot);
+	schemefd = maketempfile(schemefilename, scheme, schemesize);
+	if (schemefd < 0) {
+		int e = errno;
+		fputs(strerror(e), stderr);
+		return -e;
+	}
+	free(scheme);
 
-	fclose(fp);
+	Sscheme_init(0);
+	Sregister_boot_file(bootfilename);
+	Sbuild_heap(0, 0);
+	Sscheme_program(schemefilename, argc, argv);
+
+	close(bootfd);
+	close(schemefd);
 
 	return 0;
 }
