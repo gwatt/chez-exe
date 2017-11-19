c454a8ce77c44bfa5c91964955c1b4b72ebee50a
Graham Watt <gmwatt@gmail.com>
2017-11-19 16:26:07 -0500

    Run scheme with 'exec'

--- make-bootfile.sh
+++ make-bootfile.sh
@@ -6,6 +6,6 @@ csboot="$3"
 
 unset CHEZSCHEMELIBDIRS CHEZSCHEMELIBEXTS
 
-"$scheme" -q -b "$psboot" -b "$csboot" << __EOF__
+exec "$scheme" -q -b "$psboot" -b "$csboot" << __EOF__
 (make-boot-file "boot" '() "$psboot" "$csboot")
 __EOF__
