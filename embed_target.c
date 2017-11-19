40395be0d810d0b2087949f840a1ee6bdac349f3
Graham Watt <gmwatt@gmail.com>
2017-11-19 16:18:04 -0500

    Use asserts to ensure valid state

--- embed_target.c
+++ embed_target.c
@@ -1,4 +1,5 @@
 
+#include <assert.h>
 #include <elf.h>
 #include <fcntl.h>
 #include <errno.h>	
@@ -23,8 +24,8 @@
 # error "Unable to determine machine bits"
 #endif
 
-const char *bootname = "chezschemebootfile";
-const char *srcname = "schemesource";
+const char *bootsectionname = "chezschemebootfile";
+const char *schemesectionname = "schemeprogram";
 
 char *read_section_header_names(FILE *fp, Elf_Ehdr ehdr) {
 	Elf_Shdr strings;
@@ -41,31 +42,30 @@ char *read_section_header_names(FILE *fp, Elf_Ehdr ehdr) {
 size_t extract_elf_section(FILE *fp, uint16_t shnum, Elf_Off shoff, const char *names, const char *name, char **body) {
 	Elf_Shdr shdr;
 	int count = 0;
-	size_t size = 0;
+	size_t size = -1;
 	*body = 0;
 
-	fseek(fp, shoff, SEEK_SET);
+	assert(fseek(fp, shoff, SEEK_SET) == 0);
 	while(count++ < shnum) {
-		fread(&shdr, sizeof shdr, 1, fp);
+		assert(fread(&shdr, sizeof shdr, 1, fp) == 1);
 		if (strcmp(&names[shdr.sh_name], name) == 0) {
+			assert(fseek(fp, shdr.sh_offset, SEEK_SET) == 0);
 			size = shdr.sh_size;
-			*body = malloc(size);
-			fseek(fp, shdr.sh_offset, SEEK_SET);
-			fread(*body, 1, size, fp);
-			break;
+			assert((*body = malloc(size)) != 0);
+			assert(fread(*body, 1, size, fp) == size);
+                        break;
 		}
 	}
-
 	return size;
 }
 
 int maketempfile(char *template, const char *contents, size_t size) {
 	int fd;
 	fd = mkstemp(template);
-	if (fd < 0) return fd;
+	assert(fd >= 0);
 
-	write(fd, contents, size);
-	lseek(fd, 0, SEEK_SET);
+	assert(write(fd, contents, size) == size);
+	assert(lseek(fd, 0, SEEK_SET) == 0);
 	return fd;
 }
 
@@ -81,43 +81,37 @@ int main(int argc, const char **argv) {
 	int bootfd;
 	char schemefilename[] = "/tmp/schemeprogramXXXXXX";
 	int schemefd;
+        int ret;
 
 	fp = fopen("/proc/self/exe", "rb");
 	fread(&ehdr, sizeof ehdr, 1, fp);
 	s_names = read_section_header_names(fp, ehdr);
-	bootsize = extract_elf_section(fp, ehdr.e_shnum, ehdr.e_shoff, s_names, bootname, &boot);
-	schemesize = extract_elf_section(fp, ehdr.e_shnum, ehdr.e_shoff, s_names, srcname, &scheme);
+	bootsize = extract_elf_section(fp, ehdr.e_shnum, ehdr.e_shoff, s_names, bootsectionname, &boot);
+	schemesize = extract_elf_section(fp, ehdr.e_shnum, ehdr.e_shoff, s_names, schemesectionname, &scheme);
 	free(s_names);
 	fclose(fp);
 
-	if (bootsize) printf("Found boot data of size %lu\n", bootsize);
-	else printf("No boot data found\n");
-	if (schemesize) printf("Found scheme source of size %lu\n", schemesize);
-	else printf("No scheme source found\n");
+	assert(bootsize > 0);
+	if (!schemesize) {
+		fputs("No scheme program found\n", stderr);
+		free(boot);
+		return -ENOENT;
+	}
 
 	bootfd = maketempfile(bootfilename, boot, bootsize);
-	if (bootfd < 0) {
-		int e = errno;
-		fputs(strerror(e), stderr);
-		return -e;
-	}
-	free(boot);
 	schemefd = maketempfile(schemefilename, scheme, schemesize);
-	if (schemefd < 0) {
-		int e = errno;
-		fputs(strerror(e), stderr);
-		return -e;
-	}
-	free(scheme);
+
+        free(boot);
+        free(scheme);
 
 	Sscheme_init(0);
 	Sregister_boot_file(bootfilename);
 	Sbuild_heap(0, 0);
-	Sscheme_program(schemefilename, argc, argv);
+	ret = Sscheme_program(schemefilename, argc, argv);
 
 	close(bootfd);
 	close(schemefd);
 
-	return 0;
+	return ret;
 }
 
