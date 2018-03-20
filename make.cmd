0b512e4df31a5d0304b0f84521007e05d5806388
Graham Watt <gmwatt@gmail.com>
2018-03-20 08:18:27 -0400

    Slim down make.cmd

--- make.cmd
+++ make.cmd
@@ -1,18 +1,4 @@
  
  @echo off
 
-setlocal
-
-if "%scheme%" == "" (
-	echo Must specify scheme
-	goto out
-)
-
-if "%bootpath%" == "" (
-	echo Must specify bootpath
-	goto out
-)
-
 nmake /NOLOGO /F Makefile.win %*
-
-:out
