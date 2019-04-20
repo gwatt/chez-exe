7f64546ce8dbdac5b44057637ea7b8c07b99a9b7
Graham Watt <gmwatt@gmail.com>
2019-04-20 10:20:40 -0400

    Fix prototype

--- setup.h
+++ setup.h
@@ -4,4 +4,4 @@ extern const unsigned chezschemebootfile_size;
 extern const char scheme_program;
 extern const unsigned scheme_program_size;
 
-extern void setup(const char *argv0);
+extern int run_program(int argc, const char **argv, const char *bootfilename, const char *schemefilename);
